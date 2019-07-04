cddeb9659ac6be6faa20942777170e40b04b1e8d
Vasileios Psychas <vaspsych@gmail.com>
2019-07-04 10:31:58 +0200

    While do exercise

--- js/exercises/js_switch/main.js
+++ js/exercises/js_switch/main.js
@@ -70,22 +70,65 @@ let bandName = 'Pixies'
 // } else console.log(`Never heard of ${bandName}. What do the play?`)
 
 
-switch(bandName) {
-    case 'Slayer':
-        console.log('They play metal') 
-        break;
-    case 'Gorillaz':
-        console.log('They play jungle music') 
-        break;
-    case 'Aqua':
-        console.log('BAAAARBIEEE GGUUURL') 
-        break;
-    case 'The National':
-        console.log('So sad soooooo sad but nice') 
-        break; 
-    case 'Pixies':
-        console.log('Magdalena, you are the meanest') 
-        break; 
-    default:
-        console.log(`Never heard of ${bandName}. What do the play?`)
-}
+// switch(bandName) {
+//     case 'Slayer':
+//         console.log('They play metal') 
+//         break;
+//     case 'Gorillaz':
+//         console.log('They play jungle music') 
+//         break;
+//     case 'Aqua':
+//         console.log('BAAAARBIEEE GGUUURL') 
+//         break;
+//     case 'The National':
+//         console.log('So sad soooooo sad but nice') 
+//         break; 
+//     case 'Pixies':
+//         console.log('Magdalena, you are the meanest') 
+//         break; 
+//     default:
+//         console.log(`Never heard of ${bandName}. What do the play?`)
+// }
+
+// Write a while block that prints out all the numbers from 0 till you current age.
+// In 1988 i was 0 years old.
+// In 1989 i was 1 year old.
+// Etc etc etc
+
+
+// let year = 1988;
+// let age = 0;
+
+// do {
+//     console.log(`In ${year} i was ${age} years old`)
+//     year++;
+//     age++;
+// } while (year <= 2019)
+
+// do {
+//     console.log(`In ${year} i was ${age} years old`)
+//     year++;
+//     age++;
+// } while (age <= 31)
+
+// Write a while block that returns the hour of a day (0 till 24). 
+// If the hour is 0 till 8 return 'It's still night outside'
+// If the hour is 9 till 16 return 'OHMAGORD it's hot outside'
+// If the hour is 17 till 22 return 'It's quite better actually now'
+// If the hour is 23 till 24 return 'Bye bye i am going to bed'
+
+// let hour = 0;
+
+// do {
+//     if (hour >= 0 && hour <= 8){
+//         console.log(`The hour is ${hour}: It's still night outside`)
+//     } else if (hour > 8 && hour <= 16){
+//         console.log(`The hour is ${hour}: OHMAGORD it's hot outside`)
+//     } else if (hour > 16 && hour <= 22){
+//         console.log(`The hour is ${hour}: It's quite better actually now`)
+//     } else {
+//         console.log(`The hour is ${hour}: Bye bye i am going to bed`)
+//     }
+
+//     hour++
+// } while (hour <= 24)
